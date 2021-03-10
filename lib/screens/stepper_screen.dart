8e6ccd00ce411aa4b96640b229d0e9aa6ba42877
hideyu <hsuzuki410@gmail.com>
2021-03-10 17:27:05 +0900

    feat: 🎸 stepper package is installed

--- lib/screens/stepper_screen.dart
+++ lib/screens/stepper_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:step_to_goal/screens/welcome_screen.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 class StepperScreen extends StatefulWidget {
   static String id = 'stepper_screen';
@@ -8,15 +10,77 @@ class StepperScreen extends StatefulWidget {
 }
 
 class _StepperScreenState extends State<StepperScreen> {
+  List<Widget> Tablist = [Text('Tab1'), Text('Tab2'), Text('Tab3')];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Step To Goal"),
-      ),
-      body: Container(
-        child: Text('fugafuga'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70.0),
+          child: AppBar(
+              title: Text("Step To Goal"),
+              // actions: [],
+              bottom: TabBar(tabs: Tablist)),
+        ),
+        body: TabBarView(
+          children: [
+            StepperBodyScreen(),
+            StepperBodyScreen(),
+            StepperBodyScreen(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class StepperBodyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        StepperTimelineTile(),
+        // RaisedButton(
+        //   child: Text('test'),
+        //   onPressed: () {
+        //     Navigator.pushNamed(context, WelcomeScreen.id);
+        //   },
+        // ),
+      ],
+    );
+  }
+}
+
+class StepperTimelineTile extends StatelessWidget {
+  const StepperTimelineTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TimelineTile(
+        indicatorStyle: IndicatorStyle(
+          color: Colors.blue,
+          width: 20,
+        ),
+        endChild: Row(children: [
+          Text('hogehoge'),
+        ])
+        // endChild: Container(
+        //   height: 85.0,
+        //   child: Text('hogehgoe'),
+        //   color: Colors.green,
+        // ),
+        );
   }
 }
