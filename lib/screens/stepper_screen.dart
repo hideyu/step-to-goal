458263587f69c3079e3c7b4eb8a9f8f401594b9d
hideyu <hsuzuki410@gmail.com>
2021-03-12 14:49:37 +0900

    feat: 🎸 Data fetching from Firebase Firestore

--- lib/screens/stepper_screen.dart
+++ lib/screens/stepper_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:step_to_goal/screens/welcome_screen.dart';
+import 'package:step_to_goal/utils/firebase_helper.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class StepperScreen extends StatefulWidget {
@@ -36,36 +38,79 @@ class _StepperScreenState extends State<StepperScreen> {
   }
 }
 
-class StepperBodyScreen extends StatelessWidget {
+class StepperBodyScreen extends StatefulWidget {
+  @override
+  _StepperBodyScreenState createState() => _StepperBodyScreenState();
+}
+
+class _StepperBodyScreenState extends State<StepperBodyScreen> {
+  final _firestore = FirebaseFirestore.instance;
+  User _loggedInUser;
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        StepperTimelineTile(),
-        // RaisedButton(
-        //   child: Text('test'),
-        //   onPressed: () {
-        //     Navigator.pushNamed(context, WelcomeScreen.id);
-        //   },
-        // ),
-      ],
+    return StepperTimelineListView(
+      firestore: _firestore,
+      loggedInUser: _loggedInUser,
     );
   }
 }
 
-class StepperTimelineTile extends StatelessWidget {
-  const StepperTimelineTile({
-    Key key,
-  }) : super(key: key);
+class StepperTimelineListView extends StatelessWidget {
+  final User loggedInUser;
+  final FirebaseFirestore _firestore;
 
+  const StepperTimelineListView(
+      {@required this.loggedInUser, @required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore.collection('steps').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          final stepItems = snapshot.data.docs;
+
+          List<StepperTimelineTile> stepListWidgets = [];
+          // TODO; loggedInUserのdataだけを取得する
+          // TODO: リファクター
+          for (var stepItem in stepItems) {
+            final stepItemTitle = stepItem.data()[FirebaseDataMap.step];
+            final stepDifficultyLevel =
+                stepItem.data()[FirebaseDataMap.difficultyLevel];
+            final stepTargetDate =
+                stepItem.data()[FirebaseDataMap.targetDate].toDate();
+            final stepIsDone = stepItem.data()[FirebaseDataMap.isDone];
+
+            final stepItemWidget = StepperTimelineTile(
+                stepText: Text(
+                    '$stepItemTitle, $stepDifficultyLevel, $stepTargetDate, $stepIsDone'));
+            stepListWidgets.add(stepItemWidget);
+          }
+          return ListView(
+            children: stepListWidgets,
+          );
+        }
+        // TODO; Firebaseからのデータ取得時のエラーハンドリング
+        return Container(
+          child: Text("data fetch is failed"),
+        );
+      },
+    );
+  }
+}
+
+class StepperTimelineTile extends StatefulWidget {
+  final Widget stepText;
+
+  const StepperTimelineTile({@required this.stepText});
+
+  @override
+  _StepperTimelineTileState createState() => _StepperTimelineTileState();
+}
+
+class _StepperTimelineTileState extends State<StepperTimelineTile> {
   @override
   Widget build(BuildContext context) {
     return TimelineTile(
@@ -73,9 +118,15 @@ class StepperTimelineTile extends StatelessWidget {
           color: Colors.blue,
           width: 20,
         ),
-        endChild: Row(children: [
-          Text('hogehoge'),
-        ])
+        endChild: Row(
+          children: [
+            SizedBox(
+              width: 10,
+              height: 50,
+            ),
+            widget.stepText,
+          ],
+        )
         // endChild: Container(
         //   height: 85.0,
         //   child: Text('hogehgoe'),
---
+++ lib/utils/firebase_helper.dart
@@ -0,0 +1,105 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseDataMap {
+  static const goalTitle = 'goalTitle';
+  static const goalDate = 'goalDate';
+  static const goalIsDone = 'goalIsDone';
+  static const goalUser = 'goalUser';
+  static const step = 'step';
+  static const stepSize = 'stepSize';
+  static const difficultyLevel = 'difficultyLevel';
+  static const targetDate = 'targetDate';
+  static const isDone = 'isDone';
+  static const user = 'user';
+  static const goal = 'goal';
+}
+
+class FirebaseHelper {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  User loggedInUser;
+
+  // 2. Userがsigninしているかどうかを確認する処理
+  // TODO: dynamicでいいか不明
+  dynamic getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      // final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+        return loggedInUser;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // 3. Firestoreのデータ（Stream）を取得する処理
+  Stream getTodoItemSnapshot() {
+    final stepItemSnapshot = _firestore.collection('steps').snapshots();
+    return stepItemSnapshot;
+  }
+
+  // 3. Firestoreのデータ（Future）を取得する処理
+  dynamic getCollections(collection) async {
+    final itemCollection = await _firestore.collection(collection).get();
+    // itemCollectionにはFuture<Querysnapshot>が入る
+    // for (var item in itemCollection.docs) {
+    //   // itemCollection.docsにはDocumentSnapshotのListが格納されている
+    //   print(item.data()); // todoitem.data()で各データにアクセスできる
+    // }
+    print(itemCollection.docs.length);
+    return itemCollection;
+  }
+
+  Future<int> getCollectionLength(collection) async {
+    final itemCollection = await _firestore
+        .collection(collection)
+        .get(); // todoItemsにはFuture<Querysnapshot>が入る
+    return itemCollection.docs.length;
+  }
+
+  // 4. Firestoreにデータ（steps）を登録する処理
+  void addTodoItems({
+    String goalItem,
+    String stepItem,
+    DateTime targetDate,
+    int stepSize,
+    int difficultyLevel,
+    User loggedInUser,
+  }) async {
+    print('addTodoItems is called');
+    print('loggedInUser is $loggedInUser');
+    _firestore.collection('stepTodos').add(
+      {
+        FirebaseDataMap.goal: goalItem,
+        FirebaseDataMap.step: stepItem,
+        FirebaseDataMap.difficultyLevel: difficultyLevel,
+        FirebaseDataMap.targetDate: Timestamp.fromDate(targetDate),
+        FirebaseDataMap.stepSize: stepSize,
+        FirebaseDataMap.isDone: false,
+        FirebaseDataMap.user: loggedInUser.email,
+      },
+    );
+  }
+
+  // 5. Firestoreにデータ（goals）を登録する処理
+  void addGoalItems({
+    String goalItem,
+    DateTime goalDate,
+    User loggedInUser,
+  }) async {
+    print('addGoalItems is called');
+    print('loggedInUser is $loggedInUser');
+    _firestore.collection('goals').add(
+      {
+        FirebaseDataMap.goalTitle: goalItem,
+        FirebaseDataMap.goalDate: Timestamp.fromDate(goalDate),
+        FirebaseDataMap.goalIsDone: false,
+        FirebaseDataMap.goalUser: loggedInUser.email,
+      },
+    );
+  }
+}
